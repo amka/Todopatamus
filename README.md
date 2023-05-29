73b4f8417b5a70388dd795ad33f38b16235d69e6
Andrey Maksimov <andrey@tenderowl.com>
2023-05-29 21:18:08 +0300

    Add Tasks Page

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# todopatamus
+# Todopatamus
 
-A description of this project.
+Todopatamus is a simple and elegant todo application that helps you organize your tasks and projects. You can create lists, add items, mark them as done, edit them, delete them, and more. Todopatamus is designed to be easy to use, intuitive, and fun. You can customize the app with different themes and colors, and sync your data across devices. Todopatamus is inspired by the hippopotamus, a large and powerful animal that can accomplish anything it sets its mind to. With Todopatamus, you can achieve your goals and enjoy your life.
--- todopatamus/widgets/tasks_page.py
+++ todopatamus/widgets/tasks_page.py
@@ -25,16 +25,10 @@
 from gi.repository import Adw
 from gi.repository import Gtk
 
-from todopatamus.widgets.projects_page import ProjectsPage
-from todopatamus.widgets.tasks_page import TasksPage
 
-
-@Gtk.Template(resource_path='/com/tenderowl/todopatamus/ui/window.ui')
-class TodopatamusWindow(Adw.ApplicationWindow):
-    __gtype_name__ = 'TodopatamusWindow'
-
-    label = Gtk.Template.Child()
-    projects_page: ProjectsPage = Gtk.Template.Child()
+@Gtk.Template(resource_path='/com/tenderowl/todopatamus/ui/tasks_page.ui')
+class TasksPage(Gtk.Box):
+    __gtype_name__ = 'TasksPage'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
